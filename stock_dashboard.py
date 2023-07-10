0016121e0aca2ed481658a87a672871e91bb0efb
nvtphong <nvtphong20042001@gmmail.com>
2023-07-10 23:08:06 +0700

    eth to usd and ada to usd

--- stock_dashboard.py
+++ stock_dashboard.py
@@ -27,29 +27,68 @@ def extract_window_data(df, zero_base=True):
 app = dash.Dash()
 server = app.server
 
-scaler=MinMaxScaler(feature_range=(0,1))
-
 endpoint = 'https://min-api.cryptocompare.com/data/histoday'
-res = requests.get(endpoint + '?fsym=BTC&tsym=USD&limit=200')
 
-df = pd.DataFrame(json.loads(res.content)['Data'])
-df = df.set_index('time')
-df.index = pd.to_datetime(df.index, unit='s')
-target_col = 'close'
-df.drop(["conversionType", "conversionSymbol", "high", "low", "volumeto"], axis = 'columns', inplace = True)
+def get_chart_result(from_coin: str, to_coin: str, model_file: str):
+    res = requests.get(endpoint + '?fsym=' + from_coin + '&tsym='+ to_coin +'&limit=200')
+
+    df = pd.DataFrame(json.loads(res.content)['Data'])
+    df = df.set_index('time')
+    df.index = pd.to_datetime(df.index, unit='s')
+    target_col = 'close'
+    df.drop(["conversionType", "conversionSymbol", "high", "low", "volumeto"], axis = 'columns', inplace = True)
 
 
 
-targets = df[target_col][window_len:]
+    targets = df[target_col][window_len:]
 
-model=load_model("crypto_model.h5")
 
-X_test = extract_window_data(df)
 
-preds = model.predict(X_test).squeeze()
-# valid['Predictions']=closing_price
-preds = df[target_col].values[:-window_len] * (preds + 1)
-preds = pd.Series(index=targets.index, data=preds)
+    model=load_model(model_file)
+
+    X_test = extract_window_data(df)
+
+    preds = model.predict(X_test).squeeze()
+
+    preds = df[target_col].values[:-window_len] * (preds + 1)
+    preds = pd.Series(index=targets.index, data=preds)
+    return targets, preds
+
+def show_chart(title: str, actual, pred):
+    return dcc.Graph(
+                    figure={
+                        "data":[
+                            go.Scatter(
+                                x=actual.index,
+                                y=actual,
+                                mode='lines',
+                                text='Actual closing price',
+                                name='Actual'
+                            ),
+                            go.Scatter(
+                                x=pred.index,
+                                y=pred,
+                                mode='lines',
+                                fill='tonexty',
+                                fillcolor='rgba(167, 167, 167, 0.12)',
+                                text='Predicted closing price',
+                                name='Predicted'
+                            )
+                        ],
+                        "layout":go.Layout(
+                            title=title,
+                            xaxis={'title':'Date'},
+                            yaxis={'title':'Price (USD)'}
+                        )
+                    }
+                )
+
+
+actual_btc, pred_btc = get_chart_result(from_coin='BTC', to_coin='USD', model_file="crypto_model.h5")
+actual_eth, pred_eth = get_chart_result(from_coin='ETH', to_coin='USD', model_file="crypto_eth_model.h5")
+actual_ada, pred_ada = get_chart_result(from_coin='ADA', to_coin='USD', model_file="crypto_ada_model.h5")
+
+
 
 
 app.layout = html.Div([
@@ -61,33 +100,36 @@ app.layout = html.Div([
         dcc.Tab(children=[
             html.Div([
                 html.H2("LSTM predicted closing price",style={"textAlign": "center"}),
-                dcc.Graph(
-                    figure={
-                        "data":[
-                            go.Scatter(
-                                x=targets.index,
-                                y=targets,
-                                mode='lines',
-                                text='Actual  closing price',
-                                name='Actual'
-                            ),
-                            go.Scatter(
-                                x=preds.index,
-                                y=preds,
-                                mode='lines',
-                                fill='tonexty',
-                                fillcolor='rgba(167, 167, 167, 0.12)',
-                                text='Predicted closing price',
-                                name='Predicted'
-                            )
-                        ],
-                        "layout":go.Layout(
-                            title='BTC - USD',
-                            xaxis={'title':'Date'},
-                            yaxis={'title':'Price (USD)'}
-                        )
-                    }
-                )               
+                show_chart(title='BTC - USD', actual=actual_btc, pred=pred_btc),
+                show_chart(title='ETH - USD', actual=actual_eth, pred=pred_eth),
+                show_chart(title='ADA - USD', actual=actual_ada, pred=pred_ada)
+                # dcc.Graph(
+                #     figure={
+                #         "data":[
+                #             go.Scatter(
+                #                 x=actual_btc.index,
+                #                 y=actual_btc,
+                #                 mode='lines',
+                #                 text='Actual closing price',
+                #                 name='Actual'
+                #             ),
+                #             go.Scatter(
+                #                 x=pred_btc.index,
+                #                 y=pred_btc,
+                #                 mode='lines',
+                #                 fill='tonexty',
+                #                 fillcolor='rgba(167, 167, 167, 0.12)',
+                #                 text='Predicted closing price',
+                #                 name='Predicted'
+                #             )
+                #         ],
+                #         "layout":go.Layout(
+                #             title='BTC - USD',
+                #             xaxis={'title':'Date'},
+                #             yaxis={'title':'Price (USD)'}
+                #         )
+                #     }
+                # )               
             ])                
         ]),
         # Tab 2
